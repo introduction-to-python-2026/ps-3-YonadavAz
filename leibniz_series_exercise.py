28e4d772a038bc725d756e5ce106910aaa1ddfaa
YonadavAz <104059408+YonadavAz@users.noreply.github.com>
2025-11-12 14:18:12 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,5 +1,5 @@
 def approximate_pi(n_terms):
-    i = 0
-    for n in n_terms:
-        i += (-1)**n/(2*n+1)
-    return i*4
+    total = 0
+    for n in range(n_terms):
+        total += (-1)**n / (2*n + 1)
+    return 4 * total
